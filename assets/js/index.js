87cad9a14f1958a28fe24614af5fddfd91121045
= <isyed867@gmail.com>
2016-11-06 12:54:51 -0500

    Added kanye and coutdown timer

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function() { 
-  function goToDarkRSVP(){
-    alert("magic");
-    // document.body.className = document.body.className.replace("light","dark");
-    document.body.className = 'dark';
-    document.getElementById("light-password-section").classList.add('hide');
-
-  }
-});
